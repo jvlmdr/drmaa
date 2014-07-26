af9836967706a551fab813e5f59585ce8b61aa49
Jack Valmadre <jack.valmadre@gmail.com>
2014-07-26 16:18:51 -0400

    Add short package doc

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package drmaa wraps the Distributed Resource Management Application API for Grid Engine, Torque, PBS and similar.
+
+All returned errors are of type *drmaa.Error to satisfy the API.
+*/
+package drmaa
